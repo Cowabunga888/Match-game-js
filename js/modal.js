57331be79e48183f8edc55ae32e2b58f54411a8b
BuiMinhHieu <hieu.bm@softworld.jp>
2024-02-29 15:29:21 +0700

    hanlde mp3 src

--- js/modal.js
+++ js/modal.js
@@ -1,13 +1,13 @@
 const winningModalCotent = ()=> ({
-  messageType: 'INDEX_JS_CLICKED',
+  messageType: 'INDEX_JS_MESSAGE',
   modalType: 'WIN',
   data: {
     message: 'You win!!!'
   }
 })
 const losingModalCotent = ()=>({
-  messageType: 'INDEX_JS_CLICKED',
-  modalType: 'WIN',
+  messageType: 'INDEX_JS_MESSAGE',
+  modalType: 'LOSE',
   data: {
     message: 'You lose ಥ_ಥ'
   }
